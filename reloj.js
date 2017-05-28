62072cd323f56318b0dc76033ab2b51b792137b1
calamarte <calamarte@gmail.com>
2017-05-28 20:20:04 +0200

    reloj hacia delante listo

---
+++ reloj.js
@@ -0,0 +1,41 @@
+/**
+ * Created by calamarte on 28/05/2017.
+ */
+var h = 0;
+var m = 0;
+var s = 0;
+var goF;
+var goB;
+
+function goFront() {
+    goF = setInterval(function () {
+        s++;
+        if(s > 60)m++,s = 0;
+        if(m > 60)h++,m = 0;
+        var hh,mm,ss;
+        if(s < 10)ss = "0"+s;
+        else ss = JSON.stringify(s);
+        if(m < 10)mm = "0"+m;
+        else mm = JSON.stringify(m);
+        if(h < 10)hh = "0"+h;
+        else hh = JSON.stringify(h);
+
+        document.getElementById("time").innerHTML = "Modo libre : "+hh+":"+mm+":"+ss;
+    },1000);
+}
+
+function goBack() {
+
+}
+
+function restartTime(freeMode) {
+    if (freeMode)h=0,m=0,s=0;
+    else {}
+}
+
+function stopTime(freeMode) {
+    if (freeMode)clearInterval(goF);
+    else clearInterval(goB);
+}
+
+
