59748caa4f6936bf932ef9de7d9cc7a208d4d54a
calamarte <calamarte@gmail.com>
2017-05-29 01:07:08 +0200

    menos bugs

--- reloj.js
+++ reloj.js
@@ -11,62 +11,68 @@ var tm = false;
 
 
 function goFront() {
+    if(tm)stopTime(freeMode);
     goF = setInterval(function () {
-        s++;
-        if(s > 60)m++,s = 0;
-        if(m > 60)h++,m = 0;
-        var hh,mm,ss;
-        if(s < 10)ss = "0"+s;
-        else ss = JSON.stringify(s);
-        if(m < 10)mm = "0"+m;
-        else mm = JSON.stringify(m);
-        if(h < 10)hh = "0"+h;
-        else hh = JSON.stringify(h);
+            s++;
+            if (s > 60) m++, s = 0;
+            if (m > 60) h++, m = 0;
+            var hh, mm, ss;
+            if (s < 10) ss = "0" + s;
+            else ss = JSON.stringify(s);
+            if (m < 10) mm = "0" + m;
+            else mm = JSON.stringify(m);
+            if (h < 10) hh = "0" + h;
+            else hh = JSON.stringify(h);
 
-        document.getElementById("time").innerHTML = "Modo libre : "+hh+":"+mm+":"+ss;
-    },1000);
+            document.getElementById("time").innerHTML = "Modo libre : " + hh + ":" + mm + ":" + ss;
+        }, 1000);
     tm = true;
 }
 
 function goBack() {
+    if(tm)stopTime(freeMode);
     m = start;
-    goB = setInterval(function () {
-        if(s === 0){
-            if(m === 0){
-                if(h === 0) {
-                    document.getElementById("time").innerHTML = "Modo contrareloj : 0"+h+":0"+m+":0"+s;
-                    loseProtocol();
-                    clearInterval(goB);
-                }
-                else h--,m=60;
-            }else m--,s=60;
-        }else s--;
+        goB = setInterval(function () {
+            if (s === 0) {
+                if (m === 0) {
+                    if (h === 0) {
+                        document.getElementById("time").innerHTML = "Modo contrareloj : 0" + h + ":0" + m + ":0" + s;
+                        loseProtocol();
+                        clearInterval(goB);
+                    }
+                    else h--, m = 60;
+                } else m--, s = 60;
+            } else s--;
 
-        var hh,mm,ss;
-        if(s < 10)ss = "0"+s;
-        else ss = JSON.stringify(s);
-        if(m < 10)mm = "0"+m;
-        else mm = JSON.stringify(m);
-        if(h < 10)hh = "0"+h;
-        else hh = JSON.stringify(h);
+            var hh, mm, ss;
+            if (s < 10) ss = "0" + s;
+            else ss = JSON.stringify(s);
+            if (m < 10) mm = "0" + m;
+            else mm = JSON.stringify(m);
+            if (h < 10) hh = "0" + h;
+            else hh = JSON.stringify(h);
 
-        document.getElementById("time").innerHTML = "Modo contrareloj : "+hh+":"+mm+":"+ss;
-    },1000);
+            document.getElementById("time").innerHTML = "Modo contrareloj : " + hh + ":" + mm + ":" + ss;
+        }, 1000);
     tm = true;
 }
 
-function restartTime(freeMode) {
-    if (freeMode)h=0,m=0,s=0;
-    else {
-        stopTime(freeMode);
-        goBack();
-    }
-}
+// function restartTime(freeMode) {
+//     h=0,m=0,s=0;
+//     if (!freeMode) {
+//         stopTime(freeMode);
+//
+//         goBack();
+//     }
+// }
 
 function stopTime(freeMode) {
-    if (freeMode)clearInterval(goF);
-    else clearInterval(goB);
-    tm = false;
+    if(tm) {
+        h=0,m=0,s=0;
+        if (freeMode) clearInterval(goF);
+        else clearInterval(goB);
+        tm = false;
+    }
 }
 
 
